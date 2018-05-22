87e6182d67a6019089c0452bf84f4d0f2e6f7464
Miniwhale <bytesizewhale@gamil.com>
2018-05-22 14:05:08 -0600

    I made the background show up and started work on timer

--- AirHockey.java
+++ AirHockey.java
@@ -1,6 +1,6 @@
 import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
-
+import java.util.Timer;
 /**
  * Write a description of class AirHockey here.
  *
@@ -12,14 +12,29 @@ public class AirHockey extends UI implements KeyListener
     private GoalPost gp1,gp2;
     private Puck     p;
     private Paddle   pld1,pld2;
-    
+    private Boundaries Bd;
+
     public AirHockey()
     {
         super(false);
         setFocusable(true);
         addKeyListener(this);
-        pld1 = new  Paddle(200,50,10,500,"PaddleOne.jpg");
+        pld1 = new  Paddle(200,50,50,50,"PaddleOne.jpg");
+        pld2 = new  Paddle(700,100,100,100,"PaddleOne.jpg");
+        Bd = new Boundaries(0,0,1000,700,"hockey_rink_diagram.jpg");
         this.add(pld1);
+        this.add(pld2);
+        this.add(Bd);
+        repaint();
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run(){
+                    pld1.move();
+                    pld2.move();
+                    
+                }
+            },2*60*1000, 2*60*1000);
     }
 
     public void keyPressed(KeyEvent e){
@@ -28,17 +43,19 @@ public class AirHockey extends UI implements KeyListener
             pld1.up();
         }
         if(keyCode == KeyEvent.VK_L){
+            pld1.down();
+        }
+        if(keyCode == KeyEvent.VK_UP){
+            pld2.up();
+        }
+        if(keyCode == KeyEvent.VK_DOWN){
             pld2.down();
         }
-        /**if(keyCode == KeyEvent.VK_W){
-            gp2.up();
-        }
-        if(keyCode == KeyEvent.VK_U){
-            gp2.down();
-        }*/
         this.repaint();
     }
+
     public void keyReleased(KeyEvent e){}
+
     public void keyTyped(KeyEvent e){}
-    
+
 }
--- paddle.java
+++ paddle.java
@@ -1,11 +1,6 @@
 
 
-/**
- * Write a description of class GoalPost here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
+
 public class Paddle extends Sprite
 {
     private int deltaX,deltaY;
